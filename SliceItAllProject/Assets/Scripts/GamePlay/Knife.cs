6b5011be08b2437701bf649cdbd1bf195adea772
Kirill Grushevich <Kirillgrushevich@gmail.com>
2022-06-29 15:31:00 +0200

    complete knife main behaviour

--- SliceItAllProject/Assets/Scripts/GamePlay/Knife.cs
+++ SliceItAllProject/Assets/Scripts/GamePlay/Knife.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Core;
 using UnityEngine;
@@ -8,12 +7,23 @@ namespace GamePlay
     [RequireComponent(typeof(Rigidbody))]
     public class Knife : MonoBehaviour
     {
-        [SerializeField] private float horizontalVelocity;
-        [SerializeField] private float moveUpForce;
-        [SerializeField] private float rotationUpTorque;
-        [SerializeField] private float rotationDownMaxAngularDrag;
-        [SerializeField] private AnimationCurve rotationDownAngularDragCurve;
+        [Header("Main")]
         [SerializeField] private Rigidbody knifeRigidbody;
+        
+        [Header("Movement")]
+        [SerializeField] private float horizontalVelocity = 1.5f;
+        [SerializeField] private float moveUpForce = 5f;
+        
+        [Header("Rotation")]
+        [SerializeField] private float rotationUpTorque = 20f;
+        [SerializeField] private float rotationDownMaxAngularDrag = 6f;
+        [SerializeField] private float rotationDownMaxAngle = 30f;
+        [SerializeField] private AnimationCurve rotationDownAngularDragCurve;
+        
+        [Header("AutoRotation")]
+        [SerializeField] private float autoRotationMinHeight = 3f;
+        [SerializeField] private float freeFallingMaxTime = 0.5f;
+        
         
         private InputSystem input;
         private Coroutine rotationCoroutine;
@@ -21,6 +31,8 @@ namespace GamePlay
         private Vector3 basicPosition;
         private Quaternion basicRotation;
         private float basicAngularDrag;
+
+        private Collider hitCollider;
         
         public void Setup(InputSystem inputSystem)
         {
@@ -34,6 +46,8 @@ namespace GamePlay
             
             transform.position = basicPosition;
             transform.rotation = basicRotation;
+
+            hitCollider = null;
         }
 
         public void Release()
@@ -56,20 +70,30 @@ namespace GamePlay
             }
 
             transform.Translate(Time.deltaTime * horizontalVelocity * Vector3.forward, Space.World);
-            
-            
-            
-            //TODO First tests only
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                ResetPosition();
-            }
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            //TODO for first tests only
+            if (hitCollider != null)
+            {
+                return;
+            }
+            
             Stop();
+            hitCollider = other;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!knifeRigidbody.isKinematic)
+            {
+                return;
+            }
+
+            if (hitCollider == other)
+            {
+                hitCollider = null;
+            }
         }
 
         private void Stop()
@@ -85,30 +109,42 @@ namespace GamePlay
 
         private void RotateKnife()
         {
+            RotateKnife(true);
+        }
+
+        private void RotateKnife(bool isJump)
+        {
             if (rotationCoroutine != null)
             {
                 StopCoroutine(rotationCoroutine);
             }
 
-            rotationCoroutine = StartCoroutine(RotateKnifeProcess());
+            rotationCoroutine = StartCoroutine(RotateKnifeProcess(isJump));
         }
 
-        private IEnumerator RotateKnifeProcess()
+        private IEnumerator RotateKnifeProcess(bool isJump)
         {
             knifeRigidbody.isKinematic = false;
-            knifeRigidbody.velocity = Vector3.zero;
             
+            if (isJump)
+            {
+                knifeRigidbody.velocity = Vector3.zero;
+                knifeRigidbody.AddForce(new Vector3(0f, moveUpForce, 0f), ForceMode.Impulse);
+            }
+
+            knifeRigidbody.angularDrag = basicAngularDrag;
             knifeRigidbody.AddTorque(new Vector3(rotationUpTorque, 0f, 0f), ForceMode.Impulse);
-            knifeRigidbody.AddForce(new Vector3(0f, moveUpForce, 0f), ForceMode.Impulse);
-            
+
             while (Vector3.SignedAngle(transform.forward, Vector3.up, Vector3.up) > 10f)
             {
                 yield return new WaitForFixedUpdate();
             }
 
+            hitCollider = null;
+
             var angle = Vector3.SignedAngle(transform.forward, Vector3.down, Vector3.up);
             var t = 0f;
-            while (angle > 10f)
+            while (angle > rotationDownMaxAngle)
             {
                 yield return new WaitForFixedUpdate();
                 t += Time.fixedDeltaTime;
@@ -117,11 +153,32 @@ namespace GamePlay
                 var angularDragFactor = rotationDownAngularDragCurve.Evaluate(t);
                 knifeRigidbody.angularDrag = Mathf.Lerp(basicAngularDrag, rotationDownMaxAngularDrag, angularDragFactor);
             }
+            
+            knifeRigidbody.angularDrag = rotationDownMaxAngularDrag;
+            yield return new WaitForSeconds(freeFallingMaxTime);
+
+            rotationCoroutine = null;
+            
+            var ray = new Ray(transform.position, Vector3.down);
+            if (!Physics.Raycast(ray, autoRotationMinHeight))
+            {
+                RotateKnife(false);
+            }
+
         }
 
         private void Reset()
         {
             knifeRigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            //TODO First tests only
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ResetPosition();
+            }
         }
     }
 }
