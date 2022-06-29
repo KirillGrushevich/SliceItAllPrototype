9bb655d267667e08d062be693a63f373d065b55e
Kirill Grushevich <Kirillgrushevich@gmail.com>
2022-06-29 16:20:02 +0200

    add smooth movement for camera

--- SliceItAllProject/Assets/Scripts/GamePlay/CameraController.cs
+++ SliceItAllProject/Assets/Scripts/GamePlay/CameraController.cs
@@ -1,27 +1,53 @@
-using System;
 using UnityEngine;
 
 namespace GamePlay
 {
     public class CameraController : MonoBehaviour
     {
+        [SerializeField] private float upMaxBound;
+        [SerializeField] private float downMaxBound;
+        [SerializeField] private float deadZone;
+        [SerializeField] private float verticalSmooth;
+        [SerializeField] private float maxYBoundForSmooth;
+        [SerializeField] private AnimationCurve verticalSmoothCurve;
+        
         private Transform target;
         private Vector3 offset;
-        
+
         public void Setup(Transform followTarget)
         {
             target = followTarget;
             offset = transform.position - target.position;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if (target == null)
             {
                 return;
             }
 
+            var diffY = target.position.y - transform.position.y;
+            var diffYAbs = Mathf.Abs(diffY);
+
             var pos = target.position + offset;
+            var smoothFactor = verticalSmoothCurve.Evaluate(diffYAbs / maxYBoundForSmooth);
+            pos.y = Mathf.Lerp(transform.position.y, pos.y, smoothFactor * Time.fixedDeltaTime * verticalSmooth);
+
+            if (diffYAbs > deadZone)
+            {
+                if (diffY > upMaxBound)
+                {
+                    var offsetY = diffY - upMaxBound;
+                    pos.y += offsetY;
+                }
+                else if (diffY < downMaxBound)
+                {
+                    var offsetY = downMaxBound - diffY;
+                    pos.y -= offsetY;
+                }
+            }
+
             transform.position = pos;
         }
     }
