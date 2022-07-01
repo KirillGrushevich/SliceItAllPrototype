be66ef1289cca76443a7964485970843f8e397b8
Kirill Grushevich <Kirillgrushevich@gmail.com>
2022-07-01 13:41:53 +0200

    final polishing and add apk

--- SliceItAllProject/Assets/Scripts/Editor/VertexPainter.cs
+++ SliceItAllProject/Assets/Scripts/Editor/VertexPainter.cs
@@ -5,8 +5,9 @@ namespace Editor
 {
     public class VertexPainter : EditorWindow
     {
-        private Color color = Color.white;
-        
+        private Color upColor = Color.white;
+        private Color downColor = Color.black;
+
         [MenuItem("Tools/Vertex Painter")]
         private static void ShowWindow()
         {
@@ -17,37 +18,73 @@ namespace Editor
 
         private void OnGUI()
         {
-            color = EditorGUILayout.ColorField("Color", color);
+            upColor = EditorGUILayout.ColorField("Up Color", upColor);
+            downColor = EditorGUILayout.ColorField("Down Color", downColor);
 
             if (Selection.activeObject != null && GUILayout.Button("Save"))
             {
-                foreach (var selectedObj in Selection.objects)
+                Save(false);
+            }
+            
+            if (Selection.activeObject != null && GUILayout.Button("Save invert"))
+            {
+                Save(true);
+            }
+        }
+
+        private void Save(bool invert)
+        {
+            foreach (var selectedObj in Selection.objects)
+            {
+                var obj = selectedObj as GameObject;
+                if (obj == null)
                 {
-                    var obj = selectedObj as GameObject;
-                    if (obj == null)
-                    {
-                        continue;
-                    }
-
-                    var meshFilter = obj.GetComponent<MeshFilter>();
-                    if (meshFilter == null)
-                    {
-                        continue;
-                    }
-
-                    var mesh = Instantiate(meshFilter.sharedMesh);
-                    mesh.name = $"Mesh_{color}";
-                    var colors = new Color[mesh.vertexCount];
-                    for (var i = 0; i < colors.Length; i++)
-                    {
-                        colors[i] = color;
-                    }
-
-                    mesh.colors = colors;
-                    meshFilter.sharedMesh = mesh;
-                    
-                    EditorUtility.SetDirty(meshFilter);
+                    continue;
                 }
+
+                var meshFilter = obj.GetComponent<MeshFilter>();
+                if (meshFilter == null)
+                {
+                    continue;
+                }
+
+                var mesh = Instantiate(meshFilter.sharedMesh);
+
+                var minY = 0f;
+                var maxY = 0f;
+
+                foreach (var vertex in mesh.vertices)
+                {
+                    if (vertex.y < minY)
+                    {
+                        minY = vertex.y;
+                        continue;
+                    }
+
+                    if (vertex.y > maxY)
+                    {
+                        maxY = vertex.y;
+                    }
+                }
+
+                maxY -= minY;
+
+                mesh.name = $"Mesh_{downColor}";
+                var colors = new Color[mesh.vertexCount];
+                for (var i = 0; i < colors.Length; i++)
+                {
+                    var t = (mesh.vertices[i].y + minY) / maxY;
+                    if (invert)
+                    {
+                        t = 1f - t;
+                    }
+                    colors[i] = Color.Lerp(downColor, upColor, t);
+                }
+
+                mesh.colors = colors;
+                meshFilter.sharedMesh = mesh;
+                    
+                EditorUtility.SetDirty(meshFilter);
             }
         }
     }
